6abcfa7fb959e5f8d0b736e6a36770905103733a
kianhawk09 <jimmy.jameskjh@gmail.com>
2026-04-23 12:06:16 +0100

    Added functionality for the challenge tracker

--- personal.js
+++ personal.js
@@ -1,9 +1,47 @@
-const btn = document.getElementById("button1");
+const btn1 = document.querySelector("#button1");
+const btn2 = document.querySelector("#button2");
+const btn3 = document.querySelector("#button3");
 
-let count = 0;
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
 
 // Add EventListener to btn
-btn.addEventListener("click", function () {
-  count++;
-  document.getElementById("H4_2").textContent = count + "/10";
+btn1.addEventListener("click", function () {
+    if (count1 == 0) {
+        alert("You have recycled an item!");
+    }
+    
+    if (count1 == 10) {
+        alert("Congratulations! You have completed the challenge!");
+        return;
+    }
+    count1++;
+    document.getElementById("H4_2").textContent = count1 + "/10";
+});
+
+btn2.addEventListener("click", function () {
+    if (count2 == 0) {
+        alert("You have taken the bus!");
+    }
+    
+    if (count2 == 3) {
+        alert("Congratulations! You have completed the challenge!");
+        return;
+    }
+    count2++;
+    document.getElementById("H5_2").textContent = count2 + "/3";
+});
+
+btn3.addEventListener("click", function () {
+    if (count3 == 0) {
+        alert("You have bought from a local shop!");
+    }
+    
+    if (count3 == 2) {
+        alert("Congratulations! You have completed the challenge!");
+        return;
+    }
+    count3++;
+    document.getElementById("H6_2").textContent = count3 + "/2";
 });
